2177df8a79c967890d40a87e8f35f0f065b7bca8
jk-hal <jk@hal-uae.com>
2026-05-04 15:43:59 +0530

    feat!: redesign module API

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ console.log("Hello from release-please-test v1.0.0");
 function greet(name) { return "Hello, " + name; }
 function add(a, b) { return a + b; }
 module.exports = { greet, add };
+// version check
